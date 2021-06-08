e59023a1fa0ed35f13baf54ac6dfac8f96d00e05
Luca Giozza <luca.giozza@gmail.com>
2021-06-08 10:18:02 +0200

    aggiunta funzione ultima parte secondo esercizio

--- js/script.js
+++ js/script.js
@@ -29,46 +29,92 @@
 //  (usando una funzione) Dichiariamo chi ha vinto.
 
 
-var scelta = prompt('scegli tra pari o dispari');
-var sceltaNumero = parseInt(prompt('inserisci un numero da 1 a 5'));
+// var scelta = prompt('scegli tra pari o dispari');
+// var sceltaNumero = parseInt(prompt('inserisci un numero da 1 a 5'));
 
 
-var numeroPc = getRndInteger(1, 5);
+// var numeroPc = getRndInteger(1, 5);
 
-function getRndInteger(min, max){
-    return Math.floor(Math.random() * (max - min + 1 )) + min;
-}
-console.log(scelta);
-console.log(sceltaNumero);
-console.log(numeroPc);
+// function getRndInteger(min, max){
+//     return Math.floor(Math.random() * (max - min + 1 )) + min;
+// }
+// console.log(scelta);
+// console.log(sceltaNumero);
+// console.log(numeroPc);
 
 
- var x = myFunction(sceltaNumero, numeroPc );
+//  var x = myFunction(sceltaNumero, numeroPc );
  
 
- function myFunction(a, b) {
-    var risultato = a + b
-  return risultato;
- }
- console.log(x);
+//  function myFunction(a, b) {
+//     var risultato = a + b
+//   return risultato;
+//  }
+//  console.log(x);
 
-if(x % 2 == 0   ){
-    console.log('pari')
-    if(scelta == 'pari'){
-        console.log('hai vinto')
-    }else if(scelta =! 'pari'){
-        console.log('hai perso')
-    }
-}else if( x % 2 != 0){
-    console.log('dispari')
-    if(scelta == 'dispari'){
-        console.log('hai vinto')
-    }else if(scelta =! 'dispari'){
-        console.log('hai perso')
-    }
+// if(x % 2 == 0   ){
+//     console.log('pari')
+//     if(scelta == 'pari'){
+//         console.log('hai vinto')
+//     }else if(scelta =! 'pari'){
+//         console.log('hai perso')
+//     }
+// }else if( x % 2 != 0){
+//     console.log('dispari')
+//     if(scelta == 'dispari'){
+//         console.log('hai vinto')
+//     }else if(scelta =! 'dispari'){
+//         console.log('hai perso')
+//     }
     
 
+// }
+
+
+
+
+
+var pariDispari = prompt ('pari o dispari');
+var numero = parseInt( prompt (' scegli un numero da 1 a 5'));
+
+
+var generatorePc = numeroR(1,5);
+
+var somma = sommaNumeri(numero, generatorePc);
+
+
+function numeroR(min,max){
+    return Math.floor(Math.random() * (max - min +1) ) + min;
 }
 
+function sommaNumeri(num1, num2){
+    return somma = num1 + num2;
+}
+
+// console.log(sommaNumeri(numero, generatorePc));
 
 
+// funzione per il pari o dispari
+
+function sePari(sum){
+    if(somma % 2 == 0){
+        return 'pari'
+    }else{
+        return 'dispari'
+    }
+}
+ 
+
+
+var risultato = sePari(somma);
+console.log(risultato);
+
+// document.getElementById('pari-dispari').innerHTML = somma ;
+
+if(risultato = pariDispari ){
+    document.getElementById('pari-dispari').innerHTML = 'l utente ha vinto'
+}
+
+if(risultato != pariDispari ){
+    document.getElementById('pari-dispari').innerHTML = 'l utente ha perso'
+}
